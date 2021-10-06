2fea9da9c394792d3091df60230ebaca4ef54873
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-10-06 02:31:50 -0500

    trying to delete and edits to posts

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,30 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const content = document.querySelector('input[name="content"]').value.trim();
+    console.log(content);
+
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+      
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          post_id: id,
+          content
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        alert(response.statusText);
+      }
+
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
